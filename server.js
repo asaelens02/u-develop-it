d3c3028740dab6e491f54e64ca7fea95f3600a36
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-24 19:46:01 -0500

    connecting with db

--- server.js
+++ server.js
@@ -26,9 +26,37 @@ app.get ('/', (req, res) => {
         message:'Hello World'
     });
 });
+//GET a single candidate
 
-db.query(`SELECT*FROM candidates`,(err,rows)=> {
-    console.log (rows);
+// db.query(`SELECT * FROM candidates WHERE id =1`, (err, row)=> {
+
+//     if (err) {
+//         console.log (err);
+//     }
+//     console.log(row);
+// });
+// db.query(`SELECT*FROM candidates`,(err,rows)=> {
+//     console.log (rows);
+// });
+
+//Delete a candidte
+// db.query(`DELETE FROM candidates WHERE id=?`,1, (err ,result)=> {
+//     if(err){
+//         console.log (err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+        VALUES (?,?,?,?)`;
+    const params =[1, 'Ronald','Firbank',1];
+    db.query(sql, params, (err, result)=> {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
 });
 
 //default response for any other request (not found)
